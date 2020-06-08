e8c7a18ab4c61ea9330d4b80f15e4a7ea8802f8e
Fiza Goyal <fgoyal2@illinois.edu>
2020-06-08 22:49:05 +0000

    Add particle.js for cool background

--- portfolio/src/main/webapp/main.js
+++ portfolio/src/main/webapp/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
   $('.header').height($(window).height());
 })
+
+particlesJS.load('particles-js', 'particles.js/particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
