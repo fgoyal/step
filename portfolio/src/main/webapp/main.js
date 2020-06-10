08c148c572eaa2fe6d63cf38844b55dadc982c39
Fiza Goyal <fgoyal2@illinois.edu>
2020-06-10 20:35:30 +0000

    Transfer old JS for comment integration

--- portfolio/src/main/webapp/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('.header').height($(window).height());
-})
-
-particlesJS.load('particles-js', 'particles.js/particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded');
-});
